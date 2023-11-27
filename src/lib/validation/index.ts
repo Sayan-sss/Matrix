55d99c06aa3964d76c85ab5275570ce60c580c46
Sayan Mondal <101855586+Sayan-sss@users.noreply.github.com>
2023-11-27 22:57:22 +0530

    first commit

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,24 @@
+import * as z from "zod";
+
+export const SignupValidation = z.object({
+  name: z.string().min(2, { message: "Name is too short" }),
+  username: z.string().min(2, { message: "Username is too short" }),
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters" }),
+});
+
+export const SigninValidation = z.object({
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters" }),
+});
+
+export const PostValidation = z.object({
+  caption: z.string().min(5).max(2200),
+  file: z.custom<File[]>(),
+  location: z.string().min(2).max(1000),
+  tags: z.string(),
+});
